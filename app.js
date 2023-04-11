13cafcc29988e674b01cb2ac3335f84096ef56f2
Binit <binitjha2000@gmail.com>
2023-04-11 19:21:10 +0530

    Added contact and add with mongo db

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 const express = require("express");
 const path = require("path"); 
 const app = express();
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/ContactDance'); //Datastore name ContactDance 
+}
 const port = 8000;
 
-// EXPRESS SPECIFIC STUFF
-app.use('/static', express.static('static')) // For serving static files
+// Define mongoose schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+var Contact = mongoose.model('Contact', contactSchema); // Schema name Contact
+
+// For serving static files
+app.use('/static', express.static('static')) 
 app.use(express.urlencoded())
 
-// PUG SPECIFIC STUFF
+
 app.set('view engine', 'pug') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
  
 // ENDPOINTS
 app.get('/', (req, res)=>{ 
-    const params = { }
-    res.status(200).render('index.pug', params);
+    res.status(200).render('home.pug');
+})
+
+app.get('/contact', (req, res)=>{ 
+    res.status(200).render('contact.pug');
+})
+
+
+app.post('/contact', (req, res)=>{ 
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.redirect("/")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database")
+    });
 })
 
 // START THE SERVER
 app.listen(port, ()=>{
     console.log(`The application started successfully on port ${port}`);
 });
-
-//rohit is good boy 
